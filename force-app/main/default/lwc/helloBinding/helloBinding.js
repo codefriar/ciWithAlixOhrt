606e0564e774bb7c8b422c4bc7d8a443f628e6f2
Kevin Poorman <kjp@codefriar.com>
2019-07-17 16:44:49 -0400

    this should suceed

--- force-app/main/default/lwc/helloBinding/helloBinding.js
+++ force-app/main/default/lwc/helloBinding/helloBinding.js
@@ -1,9 +1,12 @@
-import { LightningElement, track } from "lwc";
+import {
+  LightningElement,
+  track
+} from "lwc";
 
 export default class HelloBinding extends LightningElement {
   @track greeting = "World";
 
   handleChange(event) {
-    this.greeting = event.target.value;
+    this.greeting = event.target.value
   }
 }
